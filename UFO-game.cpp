40f1be371ae669d3955481ed632470035886b2f9
Seb <119541585+Sebbybobbler@users.noreply.github.com>
2022-12-19 13:41:40 +0000

    Update UFO-game.cpp

--- UFO-game.cpp
+++ UFO-game.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
-#include "UFO-game.hpp"
+#include <vector>
+#include "UFO_functions.cpp"
+
+
+void display_misses(int misses);
+void greet();
+void end_game(std::string answer, std::string codeword);
+void display_status(std::vector<char> incorrect, std::string answer);
 
 int main() {
 //Greet the player.
